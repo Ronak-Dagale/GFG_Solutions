c6b354f5bf2e7c22856673d74a9395e50e37e749
Ronak Dagale <114944774+Ronak-Dagale@users.noreply.github.com>
2024-02-25 16:36:47 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Reach a given score/reach-a-given-score.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+// Driver Code
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution
+{
+    public:
+    // Complete this function
+    long long int count(long long int n)
+    {
+    	// Your code here
+    vector<int >dp(n+1);
+    	dp[0]=1;
+    	for(auto x:{3,5,10}) 
+            for(int i=x;i<=n;i++)dp[i]+=dp[i-x];
+        return dp[n];
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		ll n;
+		cin>>n;
+		Solution obj;
+		cout<<obj.count(n)<<endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
