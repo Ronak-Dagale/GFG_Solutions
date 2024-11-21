d7a9e11972c745310affdfd0aa968d895713c208
Ronak Dagale <114944774+Ronak-Dagale@users.noreply.github.com>
2024-11-21 11:35:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Stock Buy and Sell – Multiple Transaction Allowed/stock-buy-and-sell-multiple-transaction-allowed.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+// Initial template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int maximumProfit(vector<int> &prices) {
+        // code here
+        int n=prices.size();
+        int j=0,temp,sum=0;
+ for(int i=1;i<n;i++,j++)
+ {
+   temp=prices[i]-prices[j];
+   if(temp>0)
+   sum+=temp;
+ }
+return sum;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        int n = arr.size();
+        Solution ob;
+        int res = ob.maximumProfit(arr);
+        cout << res;
+
+        cout << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
