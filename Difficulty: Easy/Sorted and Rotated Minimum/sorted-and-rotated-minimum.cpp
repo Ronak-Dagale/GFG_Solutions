4851d320ca1a75a6d129640b9e3a1c25da314720
Ronak Dagale <114944774+Ronak-Dagale@users.noreply.github.com>
2024-12-13 11:41:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Sorted and Rotated Minimum/sorted-and-rotated-minimum.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int findMin(vector<int>& arr) {
+        // complete the function here
+        int s=0,e=arr.size()-1;
+        while(s<e)
+        {
+            int mid=s+(e-s)/2;
+            
+            if(arr[mid]>arr[e])
+            s=mid+1;
+            else
+            e=mid;
+        }
+        // cout<<s<<endl;
+        return arr[s];
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+
+    Solution ob;
+    while (t--) {
+        string line;
+        getline(cin, line);
+        stringstream ss(line);
+        vector<int> nums;
+        int num;
+        while (ss >> num) {
+            nums.push_back(num);
+        }
+        cout << ob.findMin(nums) << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
