74a3d0bef546a9f10ec7a2926e52f85575d06323
Ronak Dagale <114944774+Ronak-Dagale@users.noreply.github.com>
2025-05-02 19:58:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Bitonic Point/bitonic-point.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+// User function template for C++
+class Solution {
+  public:
+    int findMaximum(vector<int> &arr) {
+        // code here
+        int s=0,e=arr.size()-1;
+        while(s<=e)
+        {
+            int mid=s+(e-s)/2;
+            if(arr[mid]<=arr[mid+1])
+            s=mid+1;
+            else
+            e=mid-1;
+        }
+        return arr[s];
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+
+    while (t--) {
+        vector<int> arr;
+        int value;
+
+        // Read the entire line of integers
+        string line;
+        // cin.ignore(); // Ignore the newline after the test case input
+        getline(cin, line);
+        stringstream ss(line);
+
+        // Parse integers and add them to the vector
+        while (ss >> value) {
+            arr.push_back(value);
+        }
+
+        Solution ob;
+        auto ans = ob.findMaximum(arr);
+        cout << ans << "\n";
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
