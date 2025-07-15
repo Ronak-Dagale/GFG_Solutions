d0bfad02f5aa3e868b5b2fad0cceaf74289e706d
Ronak Dagale <114944774+Ronak-Dagale@users.noreply.github.com>
2025-07-15 22:35:12 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Divisible by 13/divisible-by-13.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    bool divby13(string &s) {
+        // code here
+        int rem=0;
+        for(int i=0;i<s.size();i++)
+        {
+            rem=(rem*10)+(s[i]-'0');
+            rem=rem%13;
+        }
+        return rem==0;
+    }
+};
