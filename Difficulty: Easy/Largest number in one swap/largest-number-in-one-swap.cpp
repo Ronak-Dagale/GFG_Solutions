688cf6041ab68a98ebab64e7927c878cb495414a
Ronak Dagale <114944774+Ronak-Dagale@users.noreply.github.com>
2026-03-09 22:56:34 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Largest number in one swap/largest-number-in-one-swap.cpp
+++ Difficulty: Easy/Largest number in one swap/largest-number-in-one-swap.cpp
@@ -1,26 +1,27 @@
 class Solution {
   public:
+  int chk(vector<int>fre,int n,int id){
+      int res=-1;
+      for(int i=n+1;i<10;i++){
+          if(fre[i]>id) res=fre[i];
+      }
+      return res;
+  }
     string largestSwap(string &s) {
         // code here
-        int n = s.size();
-        vector<int> last(10, -1);
-
-        // store last occurrence of each digit
-        for (int i = 0; i < n; i++) {
-            last[s[i] - '0'] = i;
+        vector<int>fre(10,0);
+        for(int i=0;i<s.size();i++){
+            fre[s[i]-'0']=i;
         }
-
-        // iterate from left to right
-        for (int i = 0; i < n; i++) {
-            int curr = s[i] - '0';
-            // check if there exists a bigger digit later
-            for (int d = 9; d > curr; d--) {
-                if (last[d] > i) {
-                    swap(s[i], s[last[d]]);
-                    return s; // only one swap allowed
-                }
+        
+        for(int i=0;i<s.size();i++){
+            int ex=chk(fre,s[i]-'0',i);
+            if(ex!=-1)
+            {
+                swap(s[i],s[ex]);
+                return s;
             }
         }
-        return s; // no swap possible, return original
+        return s;
     }
 };
