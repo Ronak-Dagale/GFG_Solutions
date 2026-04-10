dc9b14865fe1639ba01b11a0f9c1d1c2291d76b3
Ronak Dagale <114944774+Ronak-Dagale@users.noreply.github.com>
2026-04-10 23:45:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sorted subsequence of size 3/sorted-subsequence-of-size-3.cpp
@@ -0,0 +1,27 @@
+class Solution {
+  public:
+    vector<int> find3Numbers(vector<int> &arr) {
+        // Code here
+        int n=arr.size();
+        vector<int>ans,left(n),right(n);
+        
+        left[0]=arr[0];
+        for(int i=1;i<n;i++){
+            left[i]=min(left[i-1],arr[i]);
+        }
+        right[n-1]=arr[n-1];
+        for(int i=n-2;i>=0;i--){
+            right[i]=max(right[i+1],arr[i]);
+        }
+        
+        for(int i=1;i<n-1;i++){
+            if(left[i-1]<arr[i] && right[i+1]>arr[i]){
+                ans.push_back(left[i-1]);
+                ans.push_back(arr[i]);
+                ans.push_back(right[i+1]);
+                break;
+            }
+        }
+        return ans;
+    }
+};
