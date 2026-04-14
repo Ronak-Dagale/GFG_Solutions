0c04733e522bf5e664e9505eeb7ee7d684001cda
Ronak Dagale <114944774+Ronak-Dagale@users.noreply.github.com>
2026-04-14 23:16:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Remove Spaces/remove-spaces.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    string removeSpaces(string& s) {
+        // code here
+        int j=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]!=' '){
+                s[j++]=s[i];
+            }
+        }
+        return s.substr(0,j);
+    }
+};
