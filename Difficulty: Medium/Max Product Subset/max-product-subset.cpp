f7cd320750a66d40b2ecfad75e9341ad4912c596
Ronak Dagale <114944774+Ronak-Dagale@users.noreply.github.com>
2026-06-01 23:30:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Max Product Subset/max-product-subset.cpp
@@ -0,0 +1,30 @@
+// User function Template for C++
+class Solution {
+  public:
+    long long int findMaxProduct(vector<int>& arr) {
+        // Write your code here
+         int numofzero=0;
+        int mod=1e9+7;
+        long long int small=INT_MIN;
+        long long int res=1;
+        for(auto i : arr){
+            if(i==0) numofzero++;
+            else{
+                if(i<0)
+                small=max(small,(long long int)i);
+                res=(res*i)%mod;
+            }
+        }
+        
+        if(numofzero==arr.size()) return 0;
+        if(arr.size()==1 && small!=INT_MIN) return small;
+        if(arr.size()==(numofzero+1) && small!=INT_MIN){
+            return 0;
+        }
+        if(res<0){
+            res=res/small;
+        }
+        return res;
+    }
+};
+
